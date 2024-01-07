412d8ddda4307671f643193ff2436cbdd367f805
Vaibhav Jain <vaibhavjain_1@yahoo.com>
2024-01-07 15:51:08 +0530

    correcting details.

--- src/testing/Streams.java
+++ src/testing/Streams.java
@@ -1,20 +1,14 @@
 package testing;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import edu.emory.mathcs.backport.java.util.Arrays;
-import edu.emory.mathcs.backport.java.util.concurrent.atomic.AtomicLong;
 
 public class Streams {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		List<Integer> list = Arrays.asList(new Integer[]{1,2,3});
-		list.stream()
-		.forEach(l -> {System.out.println(l*2);});
+		List<Integer> list = Arrays.asList(1,2,3);
+		list.forEach(l -> {System.out.println(l*2);});
 	}
 
 }
